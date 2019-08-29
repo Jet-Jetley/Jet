5fb39990d3973ed86f3f6e842b68fcc72ded38f0
Veera Jetley <veerajetley@veeras-air.lan>
2019-08-29 11:11:49 -0400

    YASSSSS

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ function handleclick(event){
   database.push(input.value)
   input.value = '';
 }
+database.on('child_added', function(data){
+const messageDiv = document.querySelector('.messages');
+const message = document.createElement('p');
+message.innerText = data.val();
+messageDiv.appendChild(message);
+});
